378488a480d07641e9e88b114be389a269e3d8eb
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-05-23 23:27:33 +0530

    added comments

--- src/withReadMore.tsx
+++ src/withReadMore.tsx
@@ -1,6 +1,19 @@
+/**
+ * @author Bisvarup Mukherjee
+ * @date 23/05/2020
+ */
+
 import React from 'react'
 import ReadMore from './ReadMore'
 
+/**
+ * This is the HOC version of ReadMore component.
+ * Usage: 'export default withReadMore(MyComponent)'
+ *
+ *
+ * @param Component ReactNode - The component which you want to wrap around
+ * @param readMoreProps Object - Entire prop list supported by the ReadMore component
+ */
 export default function withReadMore(
   Component: typeof React.Component,
   readMoreProps = {}
